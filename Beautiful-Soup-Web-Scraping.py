26411a32565e6c0767827400c41e204a72c42d34
ashleyliu31 <liuyx@sas.upenn.edu>
2023-01-24 01:47:32 +0900

    Update Beautiful-Soup-Web-Scraping.py

--- Beautiful-Soup-Web-Scraping.py
+++ Beautiful-Soup-Web-Scraping.py
@@ -1,15 +1,16 @@
 import requests as r
+import html5lib
 from bs4 import BeautifulSoup as soup
 
 webpages=[]
 #you can change the number in range to reflect the number of total pages in a text
-#this loop loops through all the pages in the subdirectory
+#this loop loops through all the pages in the subdirectory within the range
 for i in range(1,10):
     root_url = 'https://zh.wikisource.org/wiki/[insert url subdirectory]'+ str(i)
     webpages.append(root_url)
 headers = {'User-Agent': 'Mozilla/5.0'}
 
-#this loop downloads the plain text, finds the titles of the texts, and names the downloaded file with the titles
+#this loop downloads the plain text
 with r.Session() as s:
 
     for item in webpages:
