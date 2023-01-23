118ec28b54a3bf265f8eabbb8cda21faf168b093
ashleyliu31 <liuyx@sas.upenn.edu>
2023-01-24 02:22:52 +0900

    Update NLTK-collocation.py

--- NLTK-collocation.py
+++ NLTK-collocation.py
@@ -4,16 +4,19 @@ import os
 import nltk
 from nltk.collocations import *
 
-file_path = r'C:\Users\ashle\OneDrive\Documents\dh\chronological database\cases\case 5\corpus'
+file_path = r'[enter local directory here]'
+#this loop loops through all the txt files in the directory 
 for filename in os.listdir(file_path):
     if filename.endswith(".txt"):
        file = open("{}/{}".format(file_path,filename),encoding="utf8")
        text = file.read()
        file.close()
+       #this line uses jieba to read Chinese words and segment Chinese into two-character words  
        seg_list = jieba.cut(text, cut_all=False)
-       #print("Full Mode: " + "/ ".join(seg_list))  # 全模式
+       #this line uses NLTK's built-in bigram collocation function to find characters highly associated with each other
        bigram_measures = nltk.collocations.BigramAssocMeasures()
-       keyword_filter = lambda *w: '小說' not in w
+       #this line allows you to specify the keyword you are looking for    
+       keyword_filter = lambda *w: '[enter keyword here]' not in w
        finder = BigramCollocationFinder.from_words(seg_list)
        finder.apply_ngram_filter(keyword_filter)
        bigrams=finder.nbest(bigram_measures.likelihood_ratio, 5)
